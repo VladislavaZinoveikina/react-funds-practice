f1df77bb30971e20fe109304d5b54076830f560e
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-07-30 23:07:07 +0200

    css styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - programming language 
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
