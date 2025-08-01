d4ce5cc783468bc5b0b4f3a82175754274d3566e
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-08-01 19:45:52 +0200

    UI bib, props children

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,14 +13,27 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
 
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post name"/>
-        <input type="text" placeholder="Post description"/>
-        <button>Create post</button>
+        {/*Controlled Component */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Post name"
+        />
+        <MyInput type="text" placeholder="Post description" />
+        <MyButton type="submit" onClick={addNewPost}>Create post</MyButton>
       </form>
       <PostList posts={posts} title="Posts about JS" />
     </div>
