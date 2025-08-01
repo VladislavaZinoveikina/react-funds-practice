f95aabb28d2ca9d4fffb947fdf852e53806fdf35
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-08-01 20:24:03 +0200

    useRef => DOM el; non-controlled component

--- src/App.js
+++ src/App.js
@@ -13,26 +13,32 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
-  const [title, setTitle] = useState('');
-  const bodyInputRef = useRef();
+  const [post, setPost] = useState({title: '', body: ''});
 
   const addNewPost = (e) => {
-    e.preventDefault();
-    console.log(title);
+    e.preventDefault(); // to stop page updating when pressing the button
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''});
   };
 
 
   return (
     <div className="App">
       <form>
-        {/*Controlled Component */}
+        {/* Controlled Component: useState */}
         <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value)}
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
           type="text"
           placeholder="Post name"
         />
-        <MyInput type="text" placeholder="Post description" />
+        {/* Non-controlled Component: useRef, ref as props and React.forwardRef() */}
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
+          type="text"
+          placeholder="Post description"
+        />
         <MyButton type="submit" onClick={addNewPost}>Create post</MyButton>
       </form>
       <PostList posts={posts} title="Posts about JS" />
