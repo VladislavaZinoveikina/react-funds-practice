eac034e217635ea7522ed4d42f747e63ee751866
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-08-02 16:54:37 +0200

    sort()

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Description' },
-    { id: 2, title: 'Javascript 2', body: 'Description' },
-    { id: 3, title: 'Javascript 3', body: 'Description' },
+    { id: 1, title: 'aa', body: 'jj' },
+    { id: 2, title: 'dd 2', body: 'ss' },
+    { id: 3, title: 'bb 3', body: 'ee' },
   ]);
+  const [selectedFilter, setSelectedFilter] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -24,10 +26,27 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   };
 
+  const filterPosts = (filter) => {
+    setSelectedFilter(filter);
+    setPosts([...posts].sort((a, b) => a[filter].localeCompare(b[filter])));
+  }
+
 
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedFilter}
+          onChange={filterPosts}
+          defaultValue="Filters"
+          options={[
+            {value: 'title', name: 'Name'},
+            {value: 'body', name: 'Description'},
+          ]}
+        />
+      </div>
       {posts.length
         ?
         <PostList remove={removePost} posts={posts} title="Posts about JS" />
